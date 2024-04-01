3f7d8c025c059248696752735f3cb4a905de8f1e
mathew-tran <mathew.tran.dev@gmail.com>
2024-04-01 10:18:13 -0700

    allow escape to open / close pause menu

--- Scripts/UI/OptionsButton.gd
+++ Scripts/UI/OptionsButton.gd
@@ -4,3 +4,8 @@ extends Button
 func _on_button_up():
 	print("pressed")
 	release_focus()
+
+func _input(event):
+	if event.is_action_released("escape"):
+		if get_tree().paused == false:
+			emit_signal("button_up")
--- Scripts/UI/PauseScreen.gd
+++ Scripts/UI/PauseScreen.gd
@@ -1,5 +1,6 @@
 extends Panel
 
+var bCanUnpause = false
 
 func _ready():
 	$VBoxContainer/Button.connect("button_up", Callable(self,"OnResumeButtonUp"))
@@ -8,11 +9,22 @@ func _ready():
 
 func OnOptionButtonUp():
 	visible = true
+	await get_tree().process_frame
+	bCanUnpause = true
 	get_tree().paused = true
 
 func OnResumeButtonUp():
 	visible = false
+	await get_tree().process_frame
+	bCanUnpause = false
 	get_tree().paused = false
 
 func _exit_tree():
 	get_tree().paused = false
+
+func _input(event):
+	if bCanUnpause == false:
+		return
+	if event.is_action_released("escape"):
+		if get_tree().paused:
+			OnResumeButtonUp()
